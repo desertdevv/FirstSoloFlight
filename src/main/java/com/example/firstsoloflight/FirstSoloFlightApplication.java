4ee1fa1e9440e9de31101b1ecc0ec3534a3702aa
desertdevv <desertdevv@gmail.com>
2025-03-31 11:22:47 +0900

    first commit

---
+++ src/main/java/com/example/firstsoloflight/FirstSoloFlightApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstsoloflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSoloFlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSoloFlightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firstsoloflight/FirstSoloFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstsoloflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSoloFlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
